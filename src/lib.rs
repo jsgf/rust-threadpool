dcff371d4f98c0a0e58bf0fd8f3d89cba9c3f046
Jeremy Fitzhardinge <jeremy@goop.org>
2016-03-04 10:27:56 -0800

    Put both counters under a single lock
    This reduces the number of times we have to take a mutex when spawning
    work.
    
    test atomic_func_bench              ... bench:         389 ns/iter (+/- 12)
    test atomic_thread_bench            ... bench:         671 ns/iter (+/- 223)
    test func_bench                     ... bench:         447 ns/iter (+/- 26)
    test sync_func_bench                ... bench:         457 ns/iter (+/- 51)
    test threadpool_bench_atomic_c1_w1  ... bench:       1,859 ns/iter (+/- 945)
    test threadpool_bench_atomic_c1_w2  ... bench:       4,225 ns/iter (+/- 1,760)
    test threadpool_bench_atomic_c20_w4 ... bench:       3,788 ns/iter (+/- 1,186)
    test threadpool_bench_atomic_c2_w1  ... bench:       2,698 ns/iter (+/- 533)
    test threadpool_bench_atomic_c2_w2  ... bench:       4,835 ns/iter (+/- 1,122)
    test threadpool_bench_c1_w1         ... bench:       7,634 ns/iter (+/- 3,165)
    test threadpool_bench_c1_w2         ... bench:       8,948 ns/iter (+/- 365)
    test threadpool_bench_c20_w4        ... bench:       8,948 ns/iter (+/- 480)
    test threadpool_bench_c2_w1         ... bench:       7,342 ns/iter (+/- 1,242)
    test threadpool_bench_c2_w2         ... bench:       9,358 ns/iter (+/- 702)
    test work_bench                     ... bench:         368 ns/iter (+/- 8)

--- src/lib.rs
+++ src/lib.rs
@@ -26,21 +26,24 @@ impl<F: FnOnce()> FnBox for F {
 
 type Thunk<'a> = Box<FnBox + Send + 'a>;
 
+#[derive(Debug, Copy, Clone)]
+struct Counters {
+    count: usize,
+    count_max: usize,
+}
+
 struct Sentinel<'a> {
     jobs: &'a Arc<Mutex<Receiver<Thunk<'static>>>>,
-    thread_counter: &'a Arc<Mutex<usize>>,
-    thread_count_max: &'a Arc<Mutex<usize>>,
+    counters: &'a Arc<Mutex<Counters>>,
     active: bool
 }
 
 impl<'a> Sentinel<'a> {
     fn new(jobs: &'a Arc<Mutex<Receiver<Thunk<'static>>>>,
-           thread_counter: &'a Arc<Mutex<usize>>,
-           thread_count_max: &'a Arc<Mutex<usize>>) -> Sentinel<'a> {
+           counters: &'a Arc<Mutex<Counters>>) -> Sentinel<'a> {
         Sentinel {
             jobs: jobs,
-            thread_counter: thread_counter,
-            thread_count_max: thread_count_max,
+            counters: counters,
             active: true
         }
     }
@@ -54,8 +57,8 @@ impl<'a> Sentinel<'a> {
 impl<'a> Drop for Sentinel<'a> {
     fn drop(&mut self) {
         if self.active {
-            *self.thread_counter.lock().unwrap() -= 1;
-            spawn_in_pool(self.jobs.clone(), self.thread_counter.clone(), self.thread_count_max.clone())
+            self.counters.lock().unwrap().count -= 1;
+            spawn_in_pool(self.jobs.clone(), self.counters.clone())
         }
     }
 }
@@ -91,8 +94,7 @@ pub struct ThreadPool {
     // quit.
     jobs: Sender<Thunk<'static>>,
     job_receiver: Arc<Mutex<Receiver<Thunk<'static>>>>,
-    active_count: Arc<Mutex<usize>>,
-    max_count: Arc<Mutex<usize>>,
+    counters: Arc<Mutex<Counters>>,
 }
 
 impl ThreadPool {
@@ -106,19 +108,17 @@ impl ThreadPool {
 
         let (tx, rx) = channel::<Thunk<'static>>();
         let rx = Arc::new(Mutex::new(rx));
-        let active_count = Arc::new(Mutex::new(0));
-        let max_count = Arc::new(Mutex::new(threads));
+        let counts = Arc::new(Mutex::new(Counters { count: 0, count_max: threads }));
 
         // Threadpool threads
         for _ in 0..threads {
-            spawn_in_pool(rx.clone(), active_count.clone(), max_count.clone());
+            spawn_in_pool(rx.clone(), counts.clone());
         }
 
         ThreadPool {
             jobs: tx,
             job_receiver: rx.clone(),
-            active_count: active_count,
-            max_count: max_count
+            counters: counts,
         }
     }
 
@@ -131,41 +131,45 @@ impl ThreadPool {
 
     /// Returns the number of currently active threads.
     pub fn active_count(&self) -> usize {
-        *self.active_count.lock().unwrap()
+        self.counters.lock().unwrap().count
     }
 
     /// Returns the number of created threads
     pub fn max_count(&self) -> usize {
-        *self.max_count.lock().unwrap()
+        self.counters.lock().unwrap().count_max
     }
 
     /// Sets the number of threads to use as `threads`.
     /// Can be used to change the threadpool size during runtime
     pub fn set_threads(&mut self, threads: usize) {
         assert!(threads >= 1);
-        let current_max = self.max_count.lock().unwrap().clone();
-        *self.max_count.lock().unwrap() = threads;
+
+        let mut current_max = threads;
+        {
+            use std::mem;
+            let mut counters = self.counters.lock().unwrap();
+            mem::swap(&mut counters.count_max, &mut current_max)
+        }
+
         if threads > current_max {
             // Spawn new threads
             for _ in 0..(threads - current_max) {
-                spawn_in_pool(self.job_receiver.clone(), self.active_count.clone(), self.max_count.clone());
+                spawn_in_pool(self.job_receiver.clone(), self.counters.clone());
             }
         }
     }
 }
 
 fn spawn_in_pool(jobs: Arc<Mutex<Receiver<Thunk<'static>>>>,
-                 thread_counter: Arc<Mutex<usize>>,
-                 thread_count_max: Arc<Mutex<usize>>) {
+                 counts: Arc<Mutex<Counters>>) {
     thread::spawn(move || {
         // Will spawn a new thread on panic unless it is cancelled.
-        let sentinel = Sentinel::new(&jobs, &thread_counter, &thread_count_max);
+        let sentinel = Sentinel::new(&jobs, &counts);
 
         loop {
             // clone values so that the mutexes are not held
-            let thread_counter_val = thread_counter.lock().unwrap().clone();
-            let thread_count_max_val = thread_count_max.lock().unwrap().clone();
-            if thread_counter_val < thread_count_max_val {
+            let Counters { count, count_max } = *counts.lock().unwrap();
+            if count < count_max {
                 let message = {
                     // Only lock jobs for the time it takes
                     // to get a job, not run it.
@@ -175,9 +179,9 @@ fn spawn_in_pool(jobs: Arc<Mutex<Receiver<Thunk<'static>>>>,
 
                 match message {
                     Ok(job) => {
-                        *thread_counter.lock().unwrap() += 1;
+                        counts.lock().unwrap().count += 1;
                         job.call_box();
-                        *thread_counter.lock().unwrap() -= 1;
+                        counts.lock().unwrap().count -= 1;
                     },
 
                     // The Threadpool was dropped.
